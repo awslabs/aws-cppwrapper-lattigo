a3a1651c305ea59837b4949c2407d270a39b2dfc
Eric Crockett <ecrockett0@gmail.com>
2021-04-01 09:57:14 -0700

    Removed all build system code related to downloading Lattigo; the Go build system will do this automatically

--- cmake/modules/FindLattigoLib.cmake
+++
@@ -1,5 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# Download and install dependency - Lattigo.
-download_external_project("lattigo")
